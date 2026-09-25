88032b849b05831451c4e27edb98a4770ae5c894
24120038-arch <24120038@student.bdu.edu.vn>
2026-09-25 09:47:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CSV_FILE = "history.csv"
 menu = {
    "Đồ ăn": {
        "Pizza Hải Sản": 150000,"Pizza cá": 500000,
-       "Mì Ý Bò Bằm": 95000, "Bún riêu": 35000"
+       "Mì Ý Bò Bằm": 95000,"Bún riêu": 35000"
        "Burger Gà": 35000,"Bún sườn bò": 50000"
        "Salad Trộn": 50000,"Bánh canh cua": 40000"
        "Bít tết Bò Mỹ": 250000,"Sâm lạnh": 15000"
